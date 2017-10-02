ea70e50185bc139db63559a09d69013c91b56b06
krdinesh <dinesh@radiansys.com>
2017-10-02 11:55:56 +0530

    Adding Custom Header Adding Extra Scoper to read login user details

--- src/Provider/SmartRecruitersProvider.php
+++ src/Provider/SmartRecruitersProvider.php
@@ -14,43 +14,60 @@ use Krdinesh\OAuth2\Client\Provider\SmartRecruitersResourceOwner;
  *
  * @author krdinesh
  */
-class SmartRecruitersProvider extends AbstractProvider {
-
+class SmartRecruitersProvider extends AbstractProvider
+{
     use QueryBuilderTrait;
 
-    protected function getDefaultScopes() {
-        return ['candidates_create', 'jobs_read', 'candidates_read'];
+    protected function getDefaultScopes()
+    {
+        return ['candidates_create', 'jobs_read', 'candidates_read','users_read'];
     }
 
-    public function getBaseAccessTokenUrl(array $params) {
+    public function getBaseAccessTokenUrl(array $params)
+    {
         return "https://www.smartrecruiters.com/identity/oauth/token";
     }
 
-    public function getBaseAuthorizationUrl() {
+    public function getBaseAuthorizationUrl()
+    {
         return "https://www.smartrecruiters.com/identity/oauth/allow";
     }
 
-    public function getResourceOwnerDetailsUrl(AccessToken $token) {
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
         return "https://api.smartrecruiters.com/users/me";
     }
 
-    protected function buildQueryString(array $params) {
+    protected function buildQueryString(array $params)
+    {
         return urldecode(http_build_query($params, null, '&', \PHP_QUERY_RFC3986));
     }
 
-    protected function getScopeSeparator() {
+    protected function getScopeSeparator()
+    {
         return ' ';
     }
 
     //put your code here
-    protected function checkResponse(ResponseInterface $response, $data) {
-        if(isset($data['error'])) {
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        logger("logger", [$data]);
+        if (isset($data['error'])) {
             throw new IdentityProviderException($data['error_description'] ?: $response->getReasonPhrase(), $response->getStatusCode(), $response);
         }
     }
 
-    protected function createResourceOwner(array $response, AccessToken $token) {
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
         return new SmartRecruitersResourceOwner($response);
     }
 
+    protected function getAuthorizationHeaders($token = null)
+    {
+        return [
+            "Authorization" => "Bearer " . $token,
+            "Accept"        => "application/json",
+            "Content-Type"  => "application/json"
+        ];
+    }
 }
